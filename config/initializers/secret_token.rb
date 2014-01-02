5a17451e39d54d93961fd30b6ac5e882f42fc5ec
Johnny Santiago <JohnnyIn2A@Gmail.com>
2014-01-01 21:02:40 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShowTracker::Application.config.secret_key_base = '3b6164d077fa927e039bc1d62cd2e304237c2fdf3c2a1be780c4ff643595b236ded0bea27b794957a83ed35c4e054eeb488c531d1bec23799eb4c31237a08724'
